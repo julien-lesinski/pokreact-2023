1836785b47aaed029baefda8945786fdb1a6a9ad
Julien Lesinski <julien.lesinski@codein.fr>
2023-04-14 16:40:18 +0200

    ✨ Loops

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,31 @@ import { Color } from "./models/color";
 import { ColorChip } from "./components/atoms/ColorChip/ColorChip";
 
 function App() {
-  const color: Color = Color.Blue;
+  const colors = drawSeveralRandomColors(5);
 
   return (
     <div className="App flex flex-col items-center justify-center">
-      <div className="max-w-5xl flex flex-col items-start text-left gap-4">
-        <p>Current value: {extractColorName(color)}</p>
-        <ColorChip color={color} />
+      <div className="max-w-5xl flex items-start text-left gap-4">
+        {colors.map((color, index) => (
+          <ColorChip key={index} color={color} />
+        ))}
       </div>
     </div>
   );
 }
 
-function extractColorName(color: Color): string {
-  switch (color) {
-    case Color.Red:
-      return "RED";
-    case Color.Green:
-      return "GREEN";
-    case Color.Blue:
-      return "BLUE";
-    default:
-      throw new Error("unknown color");
+function drawRandomColor(): Color {
+  const colors: Color[] = [Color.Red, Color.Green, Color.Blue];
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
+
+function drawSeveralRandomColors(count: number): Color[] {
+  const colors: Color[] = [];
+  for (let i = 0; i < count; ++i) {
+    colors.push(drawRandomColor());
   }
+  return colors;
 }
 
 export default App;

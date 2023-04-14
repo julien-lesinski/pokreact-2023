2a1bfc29e64c16737f13223db64abc26a7f13f9f
Julien Lesinski <julien.lesinski@codein.fr>
2023-04-14 12:21:38 +0200

    ✨ First components.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Green, On } from "./components/atoms/green";
+import { Off, Red } from "./components/atoms/red";
+import { Blue } from "./components/atoms/blue";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="flex flex-col items-start">{/* If example */}</div>
+
+      <div className="flex items-center">
+        <Red />
+        <Green />
+        <Blue />
+      </div>
+    </div>
+  );
 }
 
 export default App;

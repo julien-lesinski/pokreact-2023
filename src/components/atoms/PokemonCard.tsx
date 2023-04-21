187ae4ce898f1fda8c20e24a80fec3de6d62ee31
Julien Lesinski <julien.lesinski@codein.fr>
2023-04-21 17:24:32 +0200

    ✨ useEffect fetch abilities.

--- src/components/atoms/PokemonCard.tsx
+++ src/components/atoms/PokemonCard.tsx
@@ -1,4 +1,8 @@
+import { MainClient } from "pokenode-ts";
+import { Ability } from "pokenode-ts";
+import { PokemonClient } from "pokenode-ts";
 import { Pokemon } from "pokenode-ts";
+import { useEffect, useState } from "react";
 
 interface PokemonCardProps {
   pokemon: Pokemon;
@@ -6,6 +10,13 @@ interface PokemonCardProps {
 
 export function PokemonCard(props: PokemonCardProps) {
   const { pokemon } = props;
+  const [abilitiesDetails, setAbilitiesDetails] = useState<
+    Ability[] | undefined
+  >();
+
+  useEffect(() => {
+    getPokemonFullAbility(pokemon).then(setAbilitiesDetails);
+  }, [pokemon]);
 
   return (
     <div className="flex flex-col rounded border items-center">
@@ -14,13 +25,54 @@ export function PokemonCard(props: PokemonCardProps) {
         <p className="uppercase font-bold">{pokemon.name}</p>
       </div>
 
-      {pokemon.sprites.front_default && (
-        <img
-          src={pokemon.sprites.front_default}
-          className="w-36 h-36"
-          alt="sprite"
-        />
-      )}
+      <div className="flex gap-4 p-4">
+        {pokemon.sprites.front_default && (
+          <img
+            src={pokemon.sprites.front_default}
+            className="w-36 h-36"
+            alt="sprite"
+          />
+        )}
+        <div className="flex flex-col gap-2">
+          {abilitiesDetails?.map((ability) => (
+            <div
+              key={ability.id}
+              className="bg-gradient-to-r from-indigo-900 to-slate-800 rounded px-1 cursor-help select-none"
+              title={
+                ability.effect_entries.find(
+                  (entry) => entry.language.name === "en"
+                )?.short_effect
+              }
+            >
+              {ability.name}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
+}
+
+async function getPokemonFullAbility(pokemon: Pokemon): Promise<Ability[]> {
+  const indexAbilities: [number, Ability][] = [];
+  const promises: Promise<unknown>[] = [];
+  const client = new PokemonClient();
+
+  let index = 0;
+  for (const partialAbility of pokemon.abilities) {
+    const currentIndex = index;
+    ++index;
+
+    promises.push(
+      client
+        .getAbilityByName(partialAbility.ability.name)
+        .then((ability) => indexAbilities.push([currentIndex, ability]))
+    );
+  }
+
+  await Promise.all(promises);
+
+  return indexAbilities
+    .sort(([aIndex], [bIndex]) => aIndex - bIndex)
+    .map(([, ability]) => ability);
 }
